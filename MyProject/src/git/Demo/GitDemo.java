52bf3bccc751f47fab5dadee6204189e4c310457
NIKITHA <nmoosape@kent.edu>
2018-08-30 18:08:19 -0400

    Added the Logic of How to Create the String

--- MyProject/src/git/Demo/GitDemo.java
+++ MyProject/src/git/Demo/GitDemo.java
@@ -1,5 +1,13 @@
 package git.Demo;
 
 public class GitDemo {
+	public static void main(String[] args) {
+		String s="Nikitha";
+		String s1=new String("Moospet");
+		char ch[]= {'n','i','k','h','i'};
+		String s3=new String(ch);
+		System.out.println(s);
+		System.out.println(s1);
+		System.out.println(s3);
 
-}
+}}
